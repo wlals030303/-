8d54682085c6ab2f8c5771021e46d1d41ab3c64f
wlals030303 <127457673+wlals030303@users.noreply.github.com>
2023-04-14 15:37:31 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,12 @@
+n1 = 2
+n2 = 3
+n3 = 4
+n4 = 5
+
+for i in range(1,10):
+    print(f"{n1} x {i} = {n1*i:2d}    {n2} x {i} = {n2*i:2d}    {n3} x {i} = {n3*i:2d}    {n4} x {i} = {n4*i:2d}")
+
+print()
+
+for i in range(1,10):
+    print(f"{(n1+4)} x {i} = {(n1+4)*i:2d}    {(n2+4)} x {i} = {(n2+4)*i:2d}    {(n3+4)} x {i} = {(n3+4)*i:2d}    {(n4+4)} x {i} = {(n4+4)*i:2d}")
